6a1ee895fcb35f9502950cc5005511c97a89e7e9
uozias <uozias@gmail.com>
2013-08-26 14:12:45 +0900

    広告スペースの追加 コンテンツ閲覧ボタンの追加

--- gen/com/webthreeapp/testbook/R.java
+++ gen/com/webthreeapp/testbook/R.java
@@ -21,10 +21,16 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ad_dummy=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int adList=0x7f080001;
+        public static final int imageView1=0x7f080002;
+        public static final int imageView2=0x7f080003;
+        public static final int imageView3=0x7f080004;
+        public static final int showBook=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -36,6 +42,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int show_book=0x7f050003;
     }
     public static final class style {
         /** 
--- src/com/webthreeapp/testbook/MainActivity.java
+++ src/com/webthreeapp/testbook/MainActivity.java
@@ -6,6 +6,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
@@ -15,7 +22,30 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		//起動時にコンテンツを表示
+		showContent();
 
+		//ボタンを押したらコンテンツを表示
+		Button showButton = (Button) findViewById(R.id.showBook);
+		showButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				showContent();
+			}
+		});
+
+		//全広告にタッチイベントを登録
+		LinearLayout linearLayout = (LinearLayout) findViewById(R.id.adList);
+		ImageView tmpView = null;
+		for (int i = 0; i < linearLayout.getChildCount(); i++){
+			tmpView = (ImageView) linearLayout.getChildAt(i);
+			tmpView.setOnTouchListener(new AdTouchListener());
+		}
+	}
+
+	//コンテンツ表示関数
+	public void showContent(){
 		Intent intent = new Intent();
 		intent.setAction("com.webthreeapp.book.VIEW");
 		Uri uri = Uri.parse("content://com.webthreeapp.testbook.assetfile/book.pdf");
@@ -31,11 +61,28 @@ public class MainActivity extends Activity {
 		}
 	}
 
+
+
+	//広告タッチイベントをもらう独自リスナー
+	private class AdTouchListener implements OnTouchListener{
+
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			Intent adIntent = new Intent();
+			adIntent.setAction(Intent.ACTION_VIEW);
+			adIntent.setData(Uri.parse("http://www.saizo-aoyagi.jp/"));
+			startActivity(adIntent);
+			return false;
+		}
+
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+
+		//getMenuInflater().inflate(R.menu.main, menu);
+		return false;
 	}
+
 
 }
