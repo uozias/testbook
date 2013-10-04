b9bcad10c72929810ddcbc43332fc27d71439106
uozias <uozias@gmail.com>
2013-10-04 12:19:35 +0900

    button color was changed

--- gen/com/webthreeapp/testbook/R.java
+++ gen/com/webthreeapp/testbook/R.java
@@ -10,6 +10,16 @@ package com.webthreeapp.testbook;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        /**   <color name="button_normal">#00000000</color> 
+         */
+        public static final int button_normal=0x7f040001;
+        public static final int button_other=0x7f040003;
+        /**   <color name="button_pressed">#FF2572AC</color> 
+         */
+        public static final int button_pressed=0x7f040002;
+        public static final int toolbar=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,35 +27,36 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ad_dummy=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int button=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int adList=0x7f080001;
-        public static final int imageView1=0x7f080002;
-        public static final int imageView2=0x7f080003;
-        public static final int imageView3=0x7f080004;
-        public static final int showBook=0x7f080000;
+        public static final int action_settings=0x7f090005;
+        public static final int adList=0x7f090001;
+        public static final int imageView1=0x7f090002;
+        public static final int imageView2=0x7f090003;
+        public static final int imageView3=0x7f090004;
+        public static final int showBook=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int book_name=0x7f050004;
-        public static final int book_name_j=0x7f050005;
-        public static final int hello_world=0x7f050002;
-        public static final int play_url=0x7f050006;
-        public static final int show_book=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int book_name=0x7f060004;
+        public static final int book_name_j=0x7f060005;
+        public static final int hello_world=0x7f060002;
+        public static final int play_url=0x7f060006;
+        public static final int show_book=0x7f060003;
     }
     public static final class style {
         /** 
@@ -69,10 +80,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
